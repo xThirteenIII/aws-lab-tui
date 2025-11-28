602b5708de5b435ce4465f6c6907ad8b43f46162
XThirteenIII <giuseppe.richetti@gmail.com>
2025-11-28 12:02:22 +0100

    add Update method

--- model/model.go
+++ model/model.go
@@ -34,8 +34,74 @@ func initialModel() model {
 
 // Init returns an initial command that performs some I/O.
 // No command is needed for now, so we return nil, which translates to no command.
-func Init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 
 	// nil means "no I/O for now, please"
 	return nil
+}
+
+// Update method is called when "things happen".
+// Its job is to look at what's happened and return an updated model in response.
+// It can also return a command to execute with it.
+// For now, when user presses keys to navigate through Tool List, it will update the model cursor.
+//
+// The "something happened" comes in the form of a tea.Msg, which can by any type (interface{}).
+// Messages are the result of some I/O that took place, such as keypresses, timer tick, or response from a server.
+//
+// To figure out the type of message, use a type switch or a type assertion.
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	switch msg := msg.(type) {
+
+	// tea.KeyMsg are automatically sent to the Update function when keys are pressed.
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up.
+		case "up", "k":
+			if m.cursor > 0 {
+
+				// Cursor goes back in the slice when moving up, not forward!
+				// [0]
+				// [1]
+				// [2]
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down.
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+
+				m.cursor++
+			}
+
+		// The "enter" key toggles
+		// the selected state for the item that the cursor is pointing at.
+		case "enter":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+
+		}
+
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// No tea.Cmd returned.
+	return m, nil
+}
+
+func (m model) View() string {
+
+	return ""
 }
